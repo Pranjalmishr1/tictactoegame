9b5ea04471e862232b94dfed132888aae1049a73
AbhishekTyagi1008 <abhishektyagi1008@gmail.com>
2024-07-11 12:53:26 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppointemntListComponent } from './appointemnt-list/appointemnt-list.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { TictactoegameComponent } from './tictactoegame/tictactoegame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointemntListComponent,
+    CalculatorComponent,
+    TictactoegameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
